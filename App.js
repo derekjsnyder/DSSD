85fdddaa9e0b8a21dca2bf8436715f98cd42e831
Derek Snyder <dereksnyder@u.boisestate.edu>
2018-12-12 21:53:21 -0700

    New Header done

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router, Link } from "@reach/router";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
+        <header className="header">
+          <div className="header__logo-box">
+                <img src="img/logo.jpg" alt="Logo" className="header__logo"/>
+          </div>
+          <div className="header__text-box">
+                <h1 className="heading-primary">
+                    <span className="heading-primary--main">Derek Snyder</span>
+                    <span className="heading-primary--sub">Portfolio</span>
+                </h1>
+
+                <a href="#section-tours" className="btn btn--white btn--animated">Portfolio</a>
+            </div>
         </header>
-        <Router>
-          //
-        </Router>
       </div>
     );
   }
